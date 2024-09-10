7781e5c275b9265b4cdf2cab4a8c898d20a8014b
Nuno Mesquita <nuon@Nunos-MBP.lan>
2024-09-10 16:43:17 +0100

    new items added

--- pages/3_EDA Workflow.py
+++ pages/3_EDA Workflow.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import os
 
-st.page_link("EDA.html", label="EDA", icon="🌎")
+st.page_link("https://redsquares.github.io/STREAMLIT/EDA.html", label="EDA Workflow", icon="🌎")
 
